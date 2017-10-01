d5ca2f5fa87fb8933d69b912321f81d413a3034a
Mark A Good <mark.alan.good@gmail.com>
2017-10-01 08:24:42 -0400

    add failing test for enumerables

---
+++ ArgumentParser.Tests/Parser.Parse/EnumerableTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ArgumentParser.Internal;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ArgumentParser
+{
+    [TestClass]
+    public class EnumerableTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var args = CommandLine.Split(@"""one"" ""two"" ""three""");
+            var parser = new Parser<Options>();
+            var results = parser.Parse(args);
+            var strings = results.Options.Strings;
+            Assert.AreEqual(3, strings.Count());
+        }
+
+        class Options
+        {
+            [Option]
+            public IEnumerable<string> Strings { get; set; }
+        }
+    }
+}
