581dfbd1c96f9d40b6b11a8800bfc5b36205b58c
Mark A Good <mark.alan.good@gmail.com>
2017-10-08 15:16:11 -0400

    refactor test

--- ArgumentParser.Tests/HelpText/EnumHelpTextTests.cs
+++ ArgumentParser.Tests/HelpText/EnumHelpTextTests.cs
@@ -1,4 +1,6 @@
+using System.Collections.Generic;
 using System.Linq;
+using ArgumentParser.Api;
 using ArgumentParser.Internal;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -11,12 +13,12 @@ namespace ArgumentParser
         [TestMethod]
         public void Enum_DisplayHelp_DefaultStyle()
         {
-            var argumentSpecifications = new ArgumentSpecifications<Options>();
-            argumentSpecifications.SetupValue(o => o.MyEnum)
+            var syntaxBuilder = new ArgumentSpecifications<Options>() as IFluentSyntaxBuilder<Options>;
+            syntaxBuilder.SetupValue(o => o.MyEnum)
                 .As(0)
                 .SetDefault(MyEnum.OptionA)
                 .WithDescription("Enum option description");
-            var helpText = new HelpTextInternal(argumentSpecifications);
+            var helpText = new HelpTextInternal((IEnumerable<IArgumentSpecification>) syntaxBuilder);
             var argumentHelpTexts = helpText.GetNamedOptionHelpText().ToArray();
             Assert.AreEqual(2, argumentHelpTexts.Length);
         }
