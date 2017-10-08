d5fde489d8a832507decb7ad5e6c0c49de8b80cf
Mark A Good <mark.alan.good@gmail.com>
2017-10-08 15:14:00 -0400

    add enum builder

---
+++ ArgumentParser/Api/IFluentEnumBuilder.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace ArgumentParser.Api
+{
+    public interface IFluentEnumBuilder<in TValue> : IFluentArgumentBuilder<IFluentEnumBuilder<TValue>>
+    {
+        IFluentEnumBuilder<TValue> Map(string longName, TValue toValue);
+        IFluentEnumBuilder<TValue> Map(char shortName, TValue toValue);
+    }
+}
